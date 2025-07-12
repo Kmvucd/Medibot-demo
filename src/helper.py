a2a3eb400f5e5b0efd961f533069e15826bc45e0
Netheeswaran <netheesarun223@gmail.com>
2025-07-12 12:54:12 +0100

    Demo changes

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,6 @@
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 
 
@@ -10,13 +10,18 @@ def load_pdf_file(data):
     loader = DirectoryLoader(data, glob="*.pdf", loader_cls=PyPDFLoader)
     documents = loader.load()
 
+    # Tag each document with the PDF filename it came from
+    for doc in documents:
+        file_path = doc.metadata.get("source", "")
+        doc.metadata["source"] = os.path.basename(file_path)
+
     return documents
 
 #Split the data into smaller chunks
 def text_split(extracted_data):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
-        chunk_overlap=200)
+        chunk_overlap=50)
     text_chunks = text_splitter.split_documents(extracted_data)
 
     return text_chunks
--- store_index.py
+++ store_index.py
@@ -1,6 +1,7 @@
 from src.helper import load_pdf_file, text_split, download_hugging_face_embeddings
 from dotenv import load_dotenv
 from pinecone.grpc import PineconeGRPC as Pinecone
+from langchian.vectorstores import Pinecone 
 from pinecone import ServerlessSpec
 from langchain_pinecone import PineconeVectorStore
 import os
